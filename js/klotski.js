483e81631a4ae09c44898cd454d5ad0c08dad17c
Alexander Berson <aberson@yahoo.com>
2023-05-19 09:25:40 +0300

    layout

--- js/klotski.js
+++ js/klotski.js
@@ -7,10 +7,34 @@ const setBoardSize = () => {
     document.documentElement.style.setProperty('--board-size', boardSize + 'px');
 }
 
+const fillBoard = () => {
+
+    let places = [1,0,3,8,11,9,13,14,16,19];
+    let cells = document.querySelectorAll('.cell');
+    let tiles = document.querySelectorAll('.tile');
+    // let style = window.getComputedStyle(tiles[0]);
+    // let matrix = new WebKitCSSMatrix(style.transform);
+
+    // if (matrix.m41 != 0) return;
+
+    for (let [i, n] of places.entries()) {
+
+        let rectTile = tiles[i].getBoundingClientRect();
+        let rectCell = cells[n].getBoundingClientRect();
+
+        let offsetLeft =  rectCell.left - rectTile.left;
+        let offsetTop =  rectCell.top - rectTile.top;
+
+        tiles[i].style.transform = `translate(${offsetLeft}px, ${offsetTop}px)`;
+    }
+}
+
 
 const init = () => {
 
     setBoardSize();
+    fillBoard();
+
 }
 
 window.onload = () => document.fonts.ready.then(init());
